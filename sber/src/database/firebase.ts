52d2c6694cc0f97f97bc6dfc22556818e037f94f
volchishka1 <romazverb18@gmail.com>
2022-05-16 12:09:59 +0300

    install firebase and start set

---
+++ sber/src/database/firebase.ts
@@ -0,0 +1,11 @@
+import {initializeApp} from 'firebase/app';
+const firebaseConfig = {
+  apiKey: 'AIzaSyC-ppAoqrvzgRNOx1jPsrYksDgwCTo1DAk',
+  authDomain: 'sberbankcopy.firebaseapp.com',
+  projectId: 'sberbankcopy',
+  storageBucket: 'sberbankcopy.appspot.com',
+  messagingSenderId: '298284380821',
+  appId: '1:298284380821:web:7da377630e0fd6fc90a19c'
+};
+
+const app = initializeApp(firebaseConfig);
